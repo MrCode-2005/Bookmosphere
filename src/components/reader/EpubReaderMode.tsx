f48a28c5b9847d50557134e78182c8b458767194
MrCode-2005 <vishnusajeev2005@gmail.com>
2026-03-03 16:53:03 +0530

    Fix EPUB layout: change from paginated (multi-column) to scrolled-doc (single column)

--- src/components/reader/EpubReaderMode.tsx
+++ src/components/reader/EpubReaderMode.tsx
@@ -161,7 +161,7 @@ export default function EpubReaderMode({
         const rendition = book.renderTo(viewerRef.current, {
             width: "100%",
             height: "100%",
-            flow: "paginated",
+            flow: "scrolled-doc",
             spread: "none",
         });
         renditionRef.current = rendition;
